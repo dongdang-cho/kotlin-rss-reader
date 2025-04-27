9af43b0a47ab859656f85525c397f8729f0d2e68
dycho <cdy235a@gmail.com>
2025-04-27 15:03:25 +0900

    feat: 기본 Feed 리스트 출력 기능 구현

---
+++ src/main/kotlin/study/view/OutputView.kt
@@ -0,0 +1,11 @@
+package study.view
+
+import study.domain.RssItem
+
+object OutputView {
+    fun printResult(list: List<RssItem>) {
+        for ((index, item) in list.withIndex()) {
+            println("[${index + 1}] ${item.title} (${item.publishedAt}) - ${item.link}  ")
+        }
+    }
+}
