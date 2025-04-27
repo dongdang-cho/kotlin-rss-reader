543048041a17885ceff80889ac777b51f572bea4
dycho <cdy235a@gmail.com>
2025-04-27 14:21:52 +0900

    chore: FeedUrls 내 구조가 다른 RSS 제거

--- src/main/kotlin/study/domain/FeedUrls.kt
+++ src/main/kotlin/study/domain/FeedUrls.kt
@@ -3,7 +3,6 @@ package study.domain
 object FeedUrls {
     const val YUN_BLOG = "https://cheese10yun.github.io/rss2.xml"
     const val JOJOLDU_BLOG = "https://jojoldu.tistory.com/rss"
-    const val NAVER_D2 = "https://d2.naver.com/d2.atom"
     const val WOOWAHAN_TECH = "https://techblog.woowahan.com/feed/"
     const val NHN_CLOUD_MEETUP = "https://meetup.toast.com/rss"
 
@@ -11,7 +10,6 @@ object FeedUrls {
         listOf(
             YUN_BLOG,
             JOJOLDU_BLOG,
-            NAVER_D2,
             WOOWAHAN_TECH,
             NHN_CLOUD_MEETUP,
         )
